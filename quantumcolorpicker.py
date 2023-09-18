462400760ee5f08bf52339f307c05789cf706249
luckyjupiter <103812284+luckyjupiter@users.noreply.github.com>
2023-09-17 19:46:27 -0700

    Create quantumcolorpicker.py
    simple color picker game testing Scott Wilbur's research

---
+++ quantumcolorpicker.py
@@ -0,0 +1,96 @@
+"""
+Quantum Color Predictor
+
+This script allows the user to predict the color that will be generated using the QWQNG API.
+The user is presented with a white square and four buttons representing different colors.
+After making a prediction, the square will change to the predicted color, and the result will be displayed.
+
+Author: [Your Name]
+Date: [Date]
+"""
+
+import win32com.client
+import tkinter as tk
+
+class RandomWalkBiasAmplifier:
+    """Implements the Random Walk Bias Amplifier technique."""
+    
+    def __init__(self, bound):
+        self.bound = bound
+        self.counter = 0
+
+    def process_bit(self, bit):
+        """Processes a bit and returns the amplified result."""
+        if bit == 1:
+            self.counter += 1
+        else:
+            self.counter -= 1
+
+        if self.counter >= self.bound:
+            self.counter = 0
+            return 1
+        elif self.counter <= -self.bound:
+            self.counter = 0
+            return 0
+
+        return None
+
+def predict_color():
+    """Predicts a color using the QWQNG API."""
+    qng = win32com.client.Dispatch("QWQNG.QNG")
+    colors = ["red", "blue", "green", "yellow"]
+    rand_index = int(qng.RandUniform * len(colors))
+    return colors[rand_index]
+
+class QuantumColorPredictor:
+    """Main class for the Quantum Color Predictor game."""
+    
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.title("Quantum Color Predictor")
+        
+        self.setup_ui()
+        self.predicted_color = predict_color()
+
+    def setup_ui(self):
+        """Sets up the user interface."""
+        self.color_display = tk.Canvas(self.window, width=200, height=200, bg="white")
+        self.color_display.pack(pady=20)
+        
+        self.colors = ["red", "blue", "green", "yellow"]
+        self.buttons = {}
+        for color in self.colors:
+            btn = tk.Button(self.window, text=color.capitalize(), bg="grey", width=15, height=2, command=lambda c=color: self.check_prediction(c))
+            btn.pack(side=tk.LEFT, padx=10)
+            self.buttons[color] = btn
+        
+        self.result_label = tk.Label(self.window, text="", font=("Arial", 16))
+        self.result_label.pack(pady=10)
+        
+        self.total_hits = 0
+        self.total_attempts = 0
+        self.stats_label = tk.Label(self.window, text="Accuracy: 0.00%", font=("Arial", 16))
+        self.stats_label.pack(pady=20)
+
+    def check_prediction(self, user_color):
+        """Checks the user's prediction and updates the UI."""
+        self.color_display.config(bg=self.predicted_color)
+        if user_color == self.predicted_color:
+            self.total_hits += 1
+            self.result_label.config(text="You guessed it right!", fg="green")
+        else:
+            self.result_label.config(text=f"Oops! The color was {self.predicted_color}.", fg="red")
+        
+        self.total_attempts += 1
+        accuracy = (self.total_hits / self.total_attempts) * 100
+        self.stats_label.config(text=f"Accuracy: {accuracy:.2f}%")
+        
+        self.predicted_color = predict_color()
+
+    def run(self):
+        """Starts the main loop of the game."""
+        self.window.mainloop()
+
+if __name__ == "__main__":
+    game = QuantumColorPredictor()
+    game.run()
