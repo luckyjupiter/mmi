90ef6f79c4eace1078f98152e381ba8c8aedab3b
Wandering Consciousness <simon@funx.jp>
2023-10-29 02:23:43 +0800

    Update pulse MeterFeeder init code

--- pulse.py
+++ pulse.py
@@ -1,74 +1,23 @@
 import tkinter as tk
 from tkinter import Canvas
-import os
-from sys import platform
-from ctypes import *
+import meterfeeder_init as mf
 import time
-
-# List of connected devices with serial numbers (key) and descriptions (value)
-devices = {}
-
-def load_library():
-    # Load the MeterFeeter library
-    global METER_FEEDER_LIB
-    if platform == "linux" or platform == "linux2":
-        # Linux
-        METER_FEEDER_LIB = cdll.LoadLibrary(os.getcwd() + '/libmeterfeeder.so')
-    elif platform == "darwin":
-        # OS X
-        METER_FEEDER_LIB = cdll.LoadLibrary(os.getcwd() + '/libmeterfeeder.dylib')
-    elif platform == "win32":
-        # Windows
-        METER_FEEDER_LIB = cdll.LoadLibrary(os.getcwd() + '/meterfeeder.dll')
-    METER_FEEDER_LIB.MF_Initialize.argtypes = c_char_p,
-    METER_FEEDER_LIB.MF_Initialize.restype = c_int
-    METER_FEEDER_LIB.MF_GetNumberGenerators.restype = c_int
-    METER_FEEDER_LIB.MF_GetBytes.argtypes = c_int, POINTER(c_ubyte), c_char_p, c_char_p,
-    METER_FEEDER_LIB.MF_RandUniform.argtypes = c_char_p, c_char_p
-    METER_FEEDER_LIB.MF_RandUniform.restype = c_double
-
-    # Make driver initialize all the connected devices
-    global med_error_reason
-    med_error_reason = create_string_buffer(256)
-    result = METER_FEEDER_LIB.MF_Initialize(med_error_reason)
-    print("MeterFeeder::MF_Initialize: result: " + str(result) + ", error (if any): ", med_error_reason.value)
-    if (len(med_error_reason.value) > 0):
-        exit(result)
-
-def get_devices():
-    # Get the number of connected devices
-    numGenerators = METER_FEEDER_LIB.MF_GetNumberGenerators()
-    print("MeterFeeder::MF_GetNumberGenerators: " + str(numGenerators) + " connected device(s)")
-
-    # Get the list of connected devices
-    generatorsListBuffers = [create_string_buffer(58) for i in range(numGenerators)]
-    generatorsListBufferPointers = (c_char_p*numGenerators)(*map(addressof, generatorsListBuffers))
-    METER_FEEDER_LIB.MF_GetListGenerators(generatorsListBufferPointers)
-    generatorsList = [str(s.value, 'utf-8') for s in generatorsListBuffers]
-    print("MeterFeeder::MF_GetListGenerators: Device serial numbers and descriptions:")
-    for i in range(numGenerators):
-        kvs = generatorsList[i].split("|")
-        devices[kvs[0]] = kvs[1]
-        print("\t" + str(kvs[0]) + "->" + kvs[1])
 
 class RandomWalkBiasAmplifier:
     def __init__(self, bound):
         self.bound = bound
         self.counter = 0
-
     def process_bit(self, bit):
         if bit == 1:
             self.counter += 1
         else:
             self.counter -= 1
-
         if self.counter >= self.bound:
             self.counter = 0
             return 1
         elif self.counter <= -self.bound:
             self.counter = 0
             return 0
-
         return None
 
 class QuantumPulse:
@@ -84,9 +33,9 @@ class QuantumPulse:
 
     def pulse(self):
         # Get a random frequency using MeterFeeder, taking the first device in the list (if multiple devices connected)
-        firstDevice = next(iter(devices))
-        frequency = METER_FEEDER_LIB.MF_RandUniform(firstDevice.encode("utf-8"), med_error_reason)
-        print("MeterFeeder::MF_RandUniform: result: " + str(frequency) + ", error (if any): ", med_error_reason.value)
+        firstDevice = next(iter(mf.devices))
+        frequency = mf.METER_FEEDER_LIB.MF_RandUniform(firstDevice.encode("utf-8"), mf.med_error_reason)
+        print("MeterFeeder::MF_RandUniform: result: " + str(frequency) + ", error (if any): ", mf.med_error_reason.value)
         
         # Apply bias amplification
         bit = int(frequency > 0.5)
@@ -109,8 +58,8 @@ class QuantumPulse:
         self.root.after(int(1000 * frequency), self.pulse)
 
 if __name__ == "__main__":
-    load_library()
-    get_devices()
+    mf.load_library()
+    mf.get_devices()
 
     root = tk.Tk()
     app = QuantumPulse(root)
